9bd94d2ba991dc725573e9f4a1c6d59a6f061a3e
Khazirov <khazirov804@mail.ru>
2025-01-21 21:20:57 +0300

    Add parameter validation

--- app.rb
+++ app.rb
@@ -1,39 +1,59 @@
 #encoding: utf-8
-require 'rubygems'
-require 'sinatra'
-require 'sinatra/reloader'
-require 'sqlite3'
 
+# Подключение необходимых библиотек
+require 'rubygems'         # Управление гемами (не обязательно в современных версиях Ruby, но включено для совместимости).
+require 'sinatra'          # Основной фреймворк для создания веб-приложений.
+require 'sinatra/reloader' # Позволяет автоматически перезагружать приложение при изменении кода (удобно в процессе разработки).
+require 'sqlite3'          # Подключение к библиотеке для работы с базой данных SQLite3.
+
+# Метод для инициализации базы данных
 def init_db
-	@db = SQLite3::Database.new 'leprosorium.db'
-	@db.results_as_hash = true
+  # Открытие или создание файла базы данных 'leprosorium.db'
+  @db = SQLite3::Database.new 'leprosorium.db'
+  @db.results_as_hash = true # Настройка возврата данных в виде хэша (удобно для работы с колонками по именам).
 end
 
+# Выполняется перед каждым запросом
 before do
-	init_db
+  init_db # Инициализация базы данных для каждого запроса (обеспечивает доступ к @db в обработчиках маршрутов).
 end
 
+# Настройка приложения (выполняется один раз при запуске)
 configure do
-	init_db
-	@db.execute 'create table if not exists Posts
+  init_db # Инициализация базы данных.
+
+  # Создание таблицы 'Posts', если она не существует
+  @db.execute <<-SQL
+    create table if not exists Posts
     (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      created_date TEXT,
-      content TEXT
-    )'
+      id INTEGER PRIMARY KEY AUTOINCREMENT, -- Уникальный идентификатор записи.
+      created_date TEXT,                    -- Дата создания записи (строка, чтобы избежать проблем с форматами дат).
+      content TEXT                          -- Текстовое содержимое записи.
+    )
+  SQL
 end
 
+# Обработчик GET-запроса на главную страницу ('/')
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+  # Использование шаблона erb с HTML-кодом для отображения ссылки.
+  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
 
+# Обработчик GET-запроса для страницы создания новой записи ('/new')
 get '/new' do
-  erb :new
+  erb :new # Отображение шаблона `views/new.erb`, предназначенного для ввода текста записи.
 end
 
-
+# Обработчик POST-запроса для страницы создания новой записи ('/new')
 post '/new' do
+  # Получение текста записи из параметров формы
+  content = params[:content]
 
-	content = params[:content]
-	erb "You typed #{content}"
+  # Если есть ошибки, перерисовываем страницу с формой записи.
+  if content.length <= 0
+  	@error = 'Type post text'
+    return erb :new # Возвращаем пользователю ту же страницу с ошибками.
+  end
+  # Простая проверка: отображение введённого текста на странице (без сохранения в базе).
+  erb "You typed #{content}" # Вывод введённого текста с использованием шаблона erb.
 end
